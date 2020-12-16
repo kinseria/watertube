48524aa5d2541af900b6153244c70270fe567e83
Glitch (hello-express) <none>
2020-12-16 02:28:37 +0000

    🏉🚘 Checkpoint ./test.js:8045597/1221

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
-(async() => {
-  const ytdl = require('ytdl-core');
-  console.log(await ytdl.getInfo("https://youtu.be/dQw4w9WgXcQ").videoDetails)
+(async () => {
+  const ytdl = require("ytdl-core");
+  var id = "dQw4w9WgXcQ";
+
+  ytdl.getInfo(id).then(info => {
+    console.log("title:", info.videoDetails.title);
+    console.log("rating:", info.player_response.videoDetails.averageRating);
+    console.log("uploaded by:", info.videoDetails.author.name);
+  });
 })();
