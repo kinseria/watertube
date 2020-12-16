9a7294fbb57cf4fcd91c67fb7ebf8858b443d6e3
Glitch (hello-express) <none>
2020-12-16 15:02:19 +0000

    🐠🎺 Checkpoint ./app.js:8045597/1540 ./views/player.ejs:8045597/384

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 app.get("/watch", (req, res) => {
-  res.set("Cache-Control", " max-age=7200")
+  res.set("Cache-Control", " max-age=7200");
   ytdl.getInfo(req.query.id).then(info => {
     res.render("player.ejs", {
       title: info.videoDetails.title,
       url: info.player_response.streamingData.formats[0].url,
-      description:info.videoDetails.simpleText
+      description: info.videoDetails.description.simpleText
     });
   });
 });
