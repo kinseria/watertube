19cf4b894a21f82b32a7f7a8ae46590845c91ae5
Glitch (hello-express) <none>
2020-12-16 16:56:46 +0000

    👽🎑 Checkpoint ./views/player.ejs:8045597/37 ./package.json:8045597/1510 ./test.js:8045597/1464

--- test.js
+++ test.js
@@ -1,11 +1,6 @@
 (async () => {
-  const ytdl = require("ytdl-core");
-  var id = "dQw4w9WgXcQ";
+  const ytsr = require("ytsr");
 
-  ytdl.getInfo(id).then(info => {
-    console.log("title:", info.videoDetails.title);
-    console.log("rating:", info.player_response.videoDetails.averageRating);
-    console.log("uploaded by:", info.videoDetails.author.name);
-    console.log("url:", info.player_response.streamingData.formats[0].url);
-  });
+  const searchResults = await ytsr("github");
+  console.log(searchResults);
 })();
