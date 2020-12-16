0d5d0d3644e1dae2c657a5f29d2ae4d08f8c81b9
Glitch (hello-express) <none>
2020-12-16 15:47:32 +0000

    🚞🍧 Checkpoint ./views/player.ejs:8045597/5 ./app.js:8045597/3811

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const ytdl = require("ytdl-core");
+const https = require("https"); // Mainly for downloads
 app.use(express.static("public"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -9,16 +10,27 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
-app.get("/watch", (req, res) => {
+app.get("/watch/:id", (req, res) => {
+  var id = req.params.id;
   res.set("Cache-Control", " max-age=7200");
-  ytdl.getInfo(req.query.id).then(info => {
+  ytdl.getInfo(id).then(info => {
     res.render("player.ejs", {
       title: info.videoDetails.title,
       url: info.player_response.streamingData.formats[0].url,
       description: info.videoDetails.description.simpleText,
       related_videos: info.related_videos,
       thumbnail: info.videoDetails.thumbnail.thumbnails[0].url,
-      views:info.videoDetails.viewCount,author:info.videoDetails.author.name
+      views: info.videoDetails.viewCount,
+      author: info.videoDetails.author.name
+    });
+  });
+});
+app.get("/download/:id", (req, res) => {
+  var id = req.params.id;
+  res.set("Cache-Control", " max-age=7200");
+  ytdl.getInfo(id).then(info => {
+    https.get(info.player_response.streamingData.formats[0].url, function(file) {
+      file.pipe(res);
     });
   });
 });
