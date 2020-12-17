820cc1b42d4e075e64cdd57377d514dda4a342d0
Glitch (hello-express) <none>
2020-12-17 19:00:18 +0000

    🎐🌪 Checkpoint ./test.js:8045597/1785 ./package.json:8045597/3317

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
-const ytsr = require("ytsr");
+const CommentScraper = require("yt-comment-scraper")
+// the two boolean variable default to true and false.
+const ytcomments = new CommentScraper()
 
-const results = ytsr("github")
-  .then(data => console.log(data.items))
-  .catch(err => console.error(err));
+for(let i = 0; i < 1; i++){
+    ytcomments.scrape_next_page_youtube_comments("dQw4w9WgXcQ").then((data) =>{
+        console.log(data);
+    }).catch((error)=>{
+        console.log(error);
+    });
+}
