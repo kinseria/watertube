3fc1ad20cc5e28185f9c905905ae284e1ceef4a3
Glitch (hello-express) <none>
2020-12-17 20:33:34 +0000

    📉⛪️ Checkpoint ./test.js:8045597/243

--- test.js
+++ test.js
@@ -2,11 +2,10 @@ const ytdl = require("ytdl-core");
 
 ytdl.getInfo("CZ9n1Bck_YY").then(info => {
   const tracks =
-    info.player_response.captions.playerCaptionsTracklistRenderer.captionTracks;
-  if (tracks && tracks.length) {
-    console.log(
-      "Found captions for",
-      tracks.map(t => t.name.simpleText).join(", ")
-    );
+    info.player_response.captions.playerCaptionsTracklistRenderer;
+console.log(tracks)
+  
+  if(typeof info.player_response.captions =){
+    
   }
 });
